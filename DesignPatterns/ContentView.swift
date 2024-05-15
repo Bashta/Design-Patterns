121768e23f22b0a2a177ddfa11094c15fb7f7449
Erison Veshi <erisonve@gmail.com>
2024-05-15 22:50:41 +0200

    Initial Commit

---
+++ DesignPatterns/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignPatterns
+//
+//  Created by Erison Veshi on 15.5.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Erison Veshi on 15.5.24.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
